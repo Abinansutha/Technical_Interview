4c8c060f16c20556738ec19c1eccf9fb7353be06
Abinansutha <abinansutha13@gmail.com>
2025-02-14 13:27:34 -0500

    Added a User Interface

--- WordGuessing.py
+++ WordGuessing.py
@@ -19,6 +19,39 @@ class Word:
             ]
         }
         self.word_list = word_dict["four-letter-words"]
+        self.target_word = self.random_choice()
+        self.attempts = 1
+        self.max_attempts = 5
 
     def random_choice(self):
         return random.choice(self.word_list)
+
+    def guess_word(self, guess):
+        if guess == self.target_word:
+            return True
+        else:
+            return False
+
+    def play_game(self):
+        print(self.target_word)
+        print(
+            "Welcome to Word Guess! You have 5 turns to guess the word. Please enter your first guess: "
+        )
+        while self.attempts <= self.max_attempts:
+            guess = input("Enter a four letter word: ")
+
+            self.attempts += 1
+
+            if self.guess_word(guess):
+                print("You got it! Amazing! ")
+                break
+
+            elif self.attempts > self.max_attempts:
+                print("Your out of turns game over")
+
+            else:
+                print("Wrong Guess Try Again! ")
+
+
+word = Word()
+word.play_game()
